2361569ed12248f3b82d022e25aabdc064e66750
Hari Sirikonda <harisirikonda22@gmail.com>
2025-01-28 22:42:44 +0530

    Added hover effect to the cards

--- moviesearch/src/components/MovieApp.js
+++ moviesearch/src/components/MovieApp.js
@@ -116,7 +116,7 @@ function MovieApp() {
                     <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
                         {movies.map((movie) => (
                             <div key={movie.imdbID} className="col">
-                                <div className="card h-100 shadow-sm">
+                                <div className="card-img card h-100 shadow-sm">
                                     {/* Movie Poster */}
                                     <img
                                         src={

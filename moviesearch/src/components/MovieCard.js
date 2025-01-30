af68914ccb53f33e70b0c9fb6b7534c5018301f9
Hari Sirikonda <harisirikonda22@gmail.com>
2025-01-30 13:51:20 +0530

    Added Share button functionality

--- moviesearch/src/components/MovieCard.js
+++ moviesearch/src/components/MovieCard.js
@@ -3,6 +3,7 @@ import ImageNotFound from '../assets/imagenotfound.png';
 import SaveButton from '../assets/save-instagram.png';
 import SavedButton from '../assets/bookmark.png';
 import Star from '../assets/star.png';
+import Share from '../assets/share.png';
 
 function MovieCard({ movie, isSaved, onSaveClick }) {
     return (
@@ -16,7 +17,7 @@ function MovieCard({ movie, isSaved, onSaveClick }) {
                     style={{ height: "445px", objectFit: "cover" }}
                 />
                 {/* Rating  */}
-                <div className='Rating-overlay d-flex rounded bg-light'>
+                <div className='Rating-overlay d-flex rounded bg-light shadow-sm'>
                     <div className='ms-2'>
                         <h6>{movie.imdbRating}</h6>
                     </div>
@@ -25,16 +26,24 @@ function MovieCard({ movie, isSaved, onSaveClick }) {
                     </div>
                 </div>
                 {/* Watch Trailer */}
-                <div className='Trailer-overlay text-center rounded bg-light'>
+                <div className='Trailer-overlay text-center rounded bg-light shadow-sm'>
                     <h6><a href='https://www.google.com' target='_blank'>Trailer</a></h6>
                 </div>
                 {/* Save Button Overlay */}
-                <div className="save-button-overlay no-select rounded bg-light position-absolute">
+                <div className="save-button-overlay no-select rounded bg-light position-absolute shadow-sm">
                     <img
                         src={isSaved ? SavedButton : SaveButton}
                         className="img-fluid"
                         alt="Save"
                         onClick={() => onSaveClick(movie.imdbID)}
+                        style={{ cursor: "pointer" }}
+                    />
+                </div>
+                <div className='share-button-overlay no-select rounded b-light position-absolute shadow-sm'>
+                    <img
+                        src={Share}
+                        className='img-fluid'
+                        alt='Share'
                         style={{ cursor: "pointer" }}
                     />
                 </div>

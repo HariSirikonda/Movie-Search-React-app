704d317343270d003913664fce234204ee1d9ac1
Hari Sirikonda <harisirikonda22@gmail.com>
2025-01-31 20:37:58 +0530

    Done with the trailer button back

--- moviesearch/src/components/MovieCard.js
+++ moviesearch/src/components/MovieCard.js
@@ -85,9 +85,9 @@ function MovieCard({ movie, isSaved, onSaveClick }) {
                     </div>
                 </div>
                 {/* Watch Trailer */}
-                {trailerFound && <div className='Trailer-overlay text-center rounded bg-light shadow-sm'>
-                    <h6><a href={trailerFound ? trailerLink : '#'} target='_blank' onClick={handleTrailerLinkClick} >Trailer</a></h6>
-                </div>}
+                <div className='Trailer-overlay text-center rounded bg-light shadow-sm'>
+                    <h6><a href={trailerLink} target='_blank' onClick={handleTrailerLinkClick} >Trailer</a></h6>
+                </div>
                 {/* Save Button Overlay */}
                 <div className="save-button-overlay no-select rounded bg-light position-absolute shadow-sm">
                     <img
